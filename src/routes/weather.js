3163e038a0850e69458ce53b3e26d876af7f27cb
polokang <polokang@163.com>
2019-01-19 18:02:36 +1000

    deploy

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const axios = require('../utils/axios');
 const weather = require('../models/Weather');
+const responseFormatter = require('../utils/responseFormatter');
+const countryValidator = require('../middlewares/countryValidator');
 
 const router = express.Router();
 const APPID = process.env.APPID;
 
-router.get('/:cc/:city', (req, res) => {
+router.get('/:cc/:city', countryValidator, (req, res, next) => {
   const { cc, city } = req.params;
   const weatherType = req.query.weatherType;
   weather
     .getData(city, cc, weatherType)
-    .then(response => {
-      res.send(response);
-    })
-    .catch(err => console.log(err));
+    .then(response => responseFormatter(res, 200, null, response))
+    .catch(err => next(err));
 });
 
 module.exports = router;
