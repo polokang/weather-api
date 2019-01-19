94efe62a8080b68bebc20c2100fa522e9f9c1cb2
polokang <polokang@163.com>
2019-01-19 15:47:59 +1000

    branch-4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const routes = require('./routes')
+require('dotenv').config();
+const routes = require('./routes');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
+const notFoundHandler = require('./middlewares/notFound');
 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(routes)
+app.use(helmet());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+} else {
+  app.use(morgan('common'));
+}
 
-app.listen(3000,() => console.log(`app listen...`))
+app.use(routes);
+app.use(notFoundHandler);
+
+app.listen(PORT, () => logger.info(`app listen on port ${PORT}`));
